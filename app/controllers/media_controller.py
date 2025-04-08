92b594445740e46e75dedac0f4fc340762f8b81b
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2025-04-08 17:04:37 -0300

    Add UI and integrate logic in media_controller

--- app/controllers/media_controller.py
+++ app/controllers/media_controller.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, HTTPException, Depends, Request
+from fastapi import APIRouter, HTTPException, Depends, Request, Form
 from pydantic import BaseModel
 from typing import List
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from app.services.send_media import send_group_media_messages
 from app.dependencies.auth import auth_required
+from app.config.group_map import GROUP_IDS
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates") # Rewrite the path in a more robust way
 
 class BulkMediaRequest(BaseModel):
     group_ids: List[str]
@@ -16,19 +20,25 @@ class BulkMediaRequest(BaseModel):
     mediatype: str
     mimetype: str
 
+@router.get("/media", response_class=HTMLResponse)
+async def show_media_form(request: Request, user=Depends(auth_required)):
+    return templates.TemplateResponse("media_message.html", {"request": request})
+
 @router.post("/media")
-def send_bulk_media(payload: BulkMediaRequest, user=Depends(auth_required)):
+async def send_bulk_media_ui(request: Request , message_text: str = Form(...), image_url: str = Form(...), image_name: str = Form(...), user=Depends(auth_required)):
     try:
+        group_ids = list(GROUP_IDS.values()) # Gets all test group IDs
         results = send_group_media_messages(
-            group_ids=payload.group_ids,
-            caption=payload.caption,
-            media_url=payload.media_url,
-            file_name=payload.file_name,
-            initial_delay=payload.initial_delay,
-            subsequent_delay=payload.subsequent_delay,
-            mediatype=payload.mediatype,
-            mimetype=payload.mimetype
+            group_ids=group_ids,
+            caption=message_text,
+            media_url=image_url,
+            file_name=image_name
         )
-        return {"status": "success", "results": results}
+        return templates.TemplateResponse("media_message.html", {
+            "request": request,
+            "form_action": request.url_for("send_bulk_media_ui"), # It dynamically constructs the URL path
+            "success": True,  # Tells the frontend when to fire the alert
+            "Result": results # Double-check !!!!!!!!!!!!!!!!!!!!!!!
+        })
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

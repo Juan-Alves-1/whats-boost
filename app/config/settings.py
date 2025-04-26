7e1bd0d6e2764855e2227eef10bd927c27246566
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2025-04-26 13:38:29 +0200

    Fixed allowed_emails as a list

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 from pydantic_settings import BaseSettings
 
 
@@ -9,7 +10,8 @@ class Settings(BaseSettings):
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
     SECRET_KEY: str
-    ALLOWED_EMAILS: str # Needs to be fixed in order to accommodate more emails
+    ALLOWED_EMAILS: List[str] # Needs to be fixed in order to accommodate more emails
+    ALLOWED_EMAILS1: List[str] # Needs to be fixed in order to accommodate more emails
 
     class Config:
         env_file = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '.env'))

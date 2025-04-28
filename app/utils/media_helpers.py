7fa63c032dacc5175a8cb424e80ac54b884c053c
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2025-04-28 14:30:04 +0200

    turbo speed for trial

--- app/utils/media_helpers.py
+++ app/utils/media_helpers.py
@@ -12,12 +12,14 @@ def get_typing_range_ms(caption: str) -> tuple[int, int]:
     fast_typer = 8 
     super_typer = 12
     copy_paste_typer = 16
+    turbo_mode_max = 22
+    turbo_mode_min = 28
     length = len(caption) 
     adjusted_length = length - 90 if length > 260 else length # Half-baked solution: imply join group links as a copy and paste action
 
     try: 
-        min_time = (adjusted_length / copy_paste_typer) * 1000
-        max_time = (adjusted_length / super_typer) * 1000
+        min_time = (adjusted_length / turbo_mode_min) * 1000
+        max_time = (adjusted_length / turbo_mode_max) * 1000
         return round(min_time), round(max_time)
     except Exception:
         logger.error("Unable to get dynamic WhatsApp presence (EVO delay)")

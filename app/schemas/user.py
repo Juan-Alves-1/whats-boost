e61ebd9f1f5651ff8ef79c88c43e02193167298c
Juan-Alves-1 <116723299+Juan-Alves-1@users.noreply.github.com>
2025-06-10 00:45:22 +0200

    add UserCreate, UserUpdate, UserRead schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from pydantic import BaseModel #, EmailStr
+
+class UserCreate(BaseModel):
+    email: str
+    full_name: str
+
+    class Config:
+        orm_mode = True
+
+class UserUpdate(BaseModel):
+    full_name: str | None = None
+    evo_instance_id: str | None = None
+    evo_api_key: str | None = None
+    cloudinary_api_key: str | None = None
+    cloudinary_secret_key: str | None = None
+    logo_public_id: str | None = None
+
+    class Config:
+        extra = "ignore" # only send fields that were explicitly set
+        orm_mode = True
+
+class UserRead(BaseModel):
+    id: int
+    email: str
+    full_name: str
+    evo_instance_id: str | None
+    evo_api_key: str     | None
+    cloudinary_api_key: str | None
+    cloudinary_secret_key: str | None
+    logo_public_id: str  | None
+    created_at: datetime
+    last_login: datetime | None
+
+    class Config:
+        orm_mode = True
